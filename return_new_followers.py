8eec1ada0ac3b9c777a7d212b224925ba6c89f64
Kostas Koutoupis <kkoutoup@gmail.com>
2020-01-09 16:36:14 +0000

    Update return_new_followers.py

--- return_new_followers.py
+++ return_new_followers.py
@@ -1,7 +1,7 @@
 #check to see if the data we're after exists on the page and retrieve it. if the container element doesn't exist then return 0 instead of raising an exception. An exception costs us the rest of the data on the page we want.
 def return_new_followers(self):
   try:
-    new_followers = self.driver.find_elements_by_xpath('//h5[text()="%s Summary"]/following-sibling::div//div//div/following-sibling::div/following-sibling::div/following-sibling::div/following-sibling::div//div[@class="DataPoint-info metric-followers"]'%self.month_year)
+    new_followers = self.driver.find_elements_by_xpath('//h5[text()="%s Summary"]/../..//div[@class="DataPoint-info metric-followers"]'%self.month_year)
     if new_followers:
       return new_followers[0].text
     else:
