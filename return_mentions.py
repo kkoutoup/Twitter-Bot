36d932faa966d8bcc27727faa5ff39892b4f6bf9
Kostas Koutoupis <kkoutoup@gmail.com>
2020-04-09 10:36:48 +0100

    Update return_mentions.py

--- return_mentions.py
+++ return_mentions.py
@@ -1,4 +1,4 @@
-#check to see if the data we're after exists on the page and retrieve it. if the container element doesn't exist then return 0 instead of raising an exception. An exception costs us the rest of the data on the page we want.
+# check to see if the data we're after exists on the page and retrieve it. if the container element doesn't exist then return 0 instead of raising an exception. An exception costs us the rest of the data on the page we want.
 def return_mentions(self):
   try:
     mentions = self.driver.find_elements_by_xpath('//h5[text()="%s Summary"]/following-sibling::div//div//div[@class="DataPoint-info metric-mentions"]'%self.month_year)
