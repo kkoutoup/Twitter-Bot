d515bd2b38e2dca5f2ca744371d01fbfd9397762
Kostas Koutoupis <kkoutoup@gmail.com>
2020-04-09 10:36:05 +0100

    Update return_followers.py

--- return_followers.py
+++ return_followers.py
@@ -1,4 +1,4 @@
-#check to see if the data we're after exists on the page and retrieve it. if the container element doesn't exist then return 0 instead of raising an exception. An exception costs us the rest of the data on the page we want.
+# check to see if the data we're after exists on the page and retrieve it. if the container element doesn't exist then return 0 instead of raising an exception. An exception costs us the rest of the data on the page we want.
 def return_followers(self):
   try:
     followers = self.driver.find_element_by_xpath('//div[text()="Followers"]/following-sibling::div')
